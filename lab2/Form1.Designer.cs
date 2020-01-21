66f731a4875c6fa2bd289616bf410512ba8de93c
StrongHaul <usermage@gmail.com>
2020-01-21 15:04:12 +0500

    Initial commit

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.z1 = new ZedGraph.ZedGraphControl();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // z1
+            // 
+            this.z1.AutoScroll = true;
+            this.z1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.z1.Location = new System.Drawing.Point(229, 0);
+            this.z1.Margin = new System.Windows.Forms.Padding(4);
+            this.z1.Name = "z1";
+            this.z1.ScrollGrace = 0D;
+            this.z1.ScrollMaxX = 0D;
+            this.z1.ScrollMaxY = 0D;
+            this.z1.ScrollMaxY2 = 0D;
+            this.z1.ScrollMinX = 0D;
+            this.z1.ScrollMinY = 0D;
+            this.z1.ScrollMinY2 = 0D;
+            this.z1.Size = new System.Drawing.Size(538, 472);
+            this.z1.TabIndex = 0;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(101, 363);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 363);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 30);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(11, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(211, 293);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 60.9137F;
+            this.Column1.HeaderText = "x";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column2.FillWeight = 113.0288F;
+            this.Column2.HeaderText = "y";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Red;
+            this.Column4.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Column4.FillWeight = 113.0288F;
+            this.Column4.HeaderText = "ax+b";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 30);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "a";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 325);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 27;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 359);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "b";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(31, 356);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(52, 20);
+            this.textBox2.TabIndex = 29;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 391);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Ошибка";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(65, 388);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(55, 20);
+            this.textBox3.TabIndex = 31;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 472);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.z1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Лаба 1. Метод наименьших квадратов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl z1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZedGraph;
+using System.Linq;
+using System.Collections;
+using System.IO;
+
+namespace lab2
+{
+	public partial class Form1 : Form
+	{
+		double[] X = new double[10];
+		double[] Y = new double[10];
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			GraphPane pane = z1.GraphPane;
+
+			z1.GraphPane.CurveList.Clear();
+			z1.IsShowPointValues = true;
+			z1.IsEnableHZoom = true;
+			z1.IsEnableVZoom = true;
+
+			pane.XAxis.Title.IsVisible = false;
+			pane.YAxis.Title.IsVisible = false;
+			pane.XAxis.Scale.IsSkipFirstLabel = true;
+			pane.XAxis.Scale.IsSkipLastLabel = true;
+			pane.XAxis.Scale.IsSkipCrossLabel = true;
+			pane.YAxis.Scale.IsSkipLastLabel = true;
+			pane.YAxis.Scale.IsSkipCrossLabel = true;
+			pane.Title.IsVisible = false;
+
+			pane.XAxis.Cross = 0.0;
+			pane.YAxis.Cross = 0.0;
+
+			//pane.XAxis.Scale.Min = X.Min();       // левая граница масштаба
+			//pane.XAxis.Scale.Max = X.Max();       // правая граница масштаба
+			//pane.YAxis.Scale.Min = Y.Min();      // По оси Y установим автоматический подбор масштаба
+			//pane.YAxis.Scale.Max = Y.Max();
+
+			pane.IsBoundedRanges = true;    // при автоматическом подборе масштаба нужно учитывать только видимый интервал графика
+
+			z1.AxisChange();
+			z1.Invalidate();
+		}
+
+		public (double, double) calcAВ(double sumx, double sumy, double sumxy, double sumxx, double n)    // функция расчета коэфф a и b
+		{
+			var ab = (A: 0.0, B: 0.0);        // кортеж
+			ab.A = (n * sumxy - sumx * sumy) / (n * sumxx - Math.Pow(sumx, 2));
+			ab.B = (sumy - ab.A * sumx) / n;
+			return ab;
+		}
+
+		public (double[], double) Yofline(double a, double b, int n)        // расчет точек Y апрокс прямой и суммарной ошибки
+		{
+			(double[], double) res = (new double[n], 0);     // кортеж (Y прямой, ошибка)
+
+			for (int i = 0; i < n; i++)
+			{
+				res.Item1[i] = a * X[i] + b;        // апроксимирующая функция
+				res.Item2 += Math.Pow(Y[i] - (a * X[i] + b), 2);    // суммарная ошибка
+
+			}
+			return res;
+		}
+
+		private void button1_Click(object sender, EventArgs e)      // построить
+		{
+			z1.GraphPane.CurveList.Clear();
+
+			double sumx = X.Sum();
+			double sumy = Y.Sum();
+			double sumxy = 0, sumxx = 0;
+			int n = X.Length;
+			for (int j = 0; j < n; j++)
+			{
+				sumxy += X[j] * Y[j];
+				sumxx += X[j] * X[j];
+			}
+
+			var ab = calcAВ(sumx, sumy, sumxy, sumxx, n);       // получаем кортех с коэф a и b
+			var Yaprox = Yofline(ab.Item1, ab.Item2, n);
+			double[] Yapr = Yaprox.Item1;                   // массив апроксимированных Y
+			double error = Yaprox.Item2;            // ошибка апроксимации
+
+			for (int i = 0; i < n; i++)
+				dataGridView1[2, i].Value = Math.Round(Yapr[i], 2);
+			textBox1.Text = Math.Round(ab.Item1, 2).ToString();       // вывести a
+			textBox2.Text = Math.Round(ab.Item2, 2).ToString();        // вывести b
+			textBox3.Text = Math.Round(error, 2).ToString();        // вывести ошибку
+
+			LineItem points, line;
+			points = z1.GraphPane.AddCurve("F(x)", X, Y, Color.Green, SymbolType.Circle);       // точки
+			points.Symbol.Fill.Color = Color.Green;         // цвет заливки
+			points.Symbol.Fill.Type = FillType.Solid;       // тип заливки
+			points.Line.IsVisible = false;        // линия невидима
+			line = z1.GraphPane.AddCurve("ax + b", X, Yapr, Color.Blue, SymbolType.None);      // апроксимирующая линия
+			line.Line.Width = 2;
+
+			z1.AxisChange();
+			z1.Invalidate();
+		}
+
+		private void Button2_Click(object sender, EventArgs e)      // выбрать файл
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+				return;
+			dataGridView1.Rows.Clear();
+			Array.Resize(ref X, 0);             // обнулили длину массива   
+			Array.Resize(ref Y, 0);
+
+			string filename = openFileDialog1.FileName;
+			StreamReader filereader = new StreamReader(filename);
+			int i = 0;
+			while (filereader.EndOfStream != true)
+			{
+				string[] line = filereader.ReadLine().Replace(" ", "").Split(new string[] { "," }, StringSplitOptions.None);     // строка файла "x, y"
+				Array.Resize(ref X, X.Length + 1);             // увеличили длину массива на 1 
+				Array.Resize(ref Y, Y.Length + 1);
+				X[i] = Convert.ToDouble(line[0]);
+				Y[i] = Convert.ToDouble(line[1]);
+				dataGridView1.Rows.Add(X[i], Y[i]);
+
+				//dataGridView1.Rows[i].Cells[0].Value = X[i];    // добавить x в таблицу
+				//dataGridView1.Rows[i].Cells[1].Value = Y[i];    // добавить y в таблицу
+				i++;
+			}
+			filereader.Close();
+		}
+
+	}
+}
